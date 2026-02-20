01189479661e926fb302cb1e96b6a5a7b79d9bcf
Malak Kulaib <malakkulaib4@gmail.com>
2026-02-20 14:55:37 +0200

    Create ascii.go

---
+++ ascii-art-web/ascii.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"os"
+	"strings"
+)
+
+const charHeight = 8
+
+func loadBanner(path string) (map[rune][]string, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	lines := strings.Split(strings.ReplaceAll(string(data), "\r\n", "\n"), "\n")
+	banner := make(map[rune][]string)
+
+	ch := 32
+	for i := 0; i < len(lines); {
+		if lines[i] == "" {
+			i++
+			continue
+		}
+		if i+charHeight > len(lines) {
+			break
+		}
+		banner[rune(ch)] = lines[i : i+charHeight]
+		i += charHeight
+		ch++
+	}
+	return banner, nil
+}
+
+func renderASCII(s string, banner map[rune][]string) string {
+	var sb strings.Builder
+	for line := 0; line < charHeight; line++ {
+		for _, ch := range s {
+			if art, ok := banner[ch]; ok {
+				sb.WriteString(art[line])
+			} else {
+				sb.WriteString("        ")
+			}
+		}
+		sb.WriteString("\n")
+	}
+	return sb.String()
+}
